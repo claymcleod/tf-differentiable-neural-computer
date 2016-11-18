46bb1696e194c42983e30b9d66a1c1c139cd8df3
Clay McLeod <clay.l.mcleod@gmail.comm>
2016-11-17 20:38:12 -0600

    latest

--- python/main.py
+++ python/main.py
@@ -13,7 +13,7 @@ if __name__=="__main__":
         print()
         download_babi(args.datadir)
         X, y = load_babi(args.datadir)
-        X = X[:, -1:, :]
+        #X = X[:, -1:, :] # for debugging
         print("== DNC ==")
         print()
         machine = DNC(X, y, summary_dir=args.summary_dir, N=11, W=10, R=1, checkpoint_file="checkpoint.cpkt")
